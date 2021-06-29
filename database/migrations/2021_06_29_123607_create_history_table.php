ff2787ccb88b1b600bbb734e604fadda0c28751e
Jamal Hassan <62602628+jshas@users.noreply.github.com>
2021-06-29 14:38:44 +0200

    (+)Migration: history

---
+++ database/migrations/2021_06_29_123607_create_history_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('history', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users');
+            $table->foreignId('medicine_id')->constrained('medicines');
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('history', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['medicine_id']);
+        });
+        Schema::dropIfExists('history');
+    }
+}
